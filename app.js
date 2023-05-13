be5ab65af72bafb6a68c1bcf2ee97a017d0bcbc4
bimbyy <125829501+bimbyy@users.noreply.github.com>
2023-05-13 14:45:12 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,32 +1,32 @@
 function searchGiphy(event) {
     // Prevent the form from submitting and reloading the page
-    event.preventDefault();
+   event.preventDefault();
     
     // Get the value of the input element
-    const searchTerm = $('#search-term').val();
+  const searchTerm = $('#search-term').val();
     
     // Make a request to GIPHY using ajax
-    $.ajax({
+  $.ajax({
     url:'http://api.giphy.com/v1/gifs/search', 
       data: {
-        q: searchTerm,
-        api_key: '9gJE3YnhPbfuxrGlnJCmtElYnAUeXMbh',
-        limit: 10
+      q: searchTerm,
+      api_key: '9gJE3YnhPbfuxrGlnJCmtElYnAUeXMbh',
+      limit: 10
       },
-    success: function(response) {
+  success: function(response) {
       // Get a random GIF from the response data
-      const gif = response.data[Math.floor(Math.random() * response.data.length)];
+    const gif = response.data[Math.floor(Math.random() * response.data.length)];
     
       // Create a new image element with the GIF URL
-      const img = $('<img>').addClass('giphy-image').attr('src', gif.images.original.url);
+    const img = $('<img>').addClass('giphy-image').attr('src', gif.images.original.url);
     
       // Append the image element to the body of the page
-      $('body').append(img);
+    $('body').append(img);
       //clear search field
-      $('#search-term').val('');
+    $('#search-term').val('');
     },
-    error: function (error){
-      console.error(error);
+  error: function (error){
+    console.error(error);
     }
   });
   }
